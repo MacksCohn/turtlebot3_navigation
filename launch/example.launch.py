028cb67c1eab1e144ce5bf7da6c0afbe1fb040d5
MacksCohn <maxcohn@live.com>
2024-06-13 11:44:24 -0400

    Full launch file

--- launch/example.launch.py
+++ launch/example.launch.py
@@ -6,9 +6,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     
     map_file = os.path.join(get_package_share_directory('turtlebot3_navigation'), 'config', 'map.yaml')
-
+    controller_yaml = os.path.join(get_package_share_directory('turtlebot3_navigation'), 'config', 'controller.yaml')
     return LaunchDescription([
-        Node(
+         Node(
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
@@ -16,7 +16,22 @@ def generate_launch_description():
             parameters=[{'use_sim_time': True}, 
                         {'yaml_filename':map_file} 
                        ]),
-
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen'),
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen'),
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            name='controller_server',
+            output='screen',
+            parameters=[{'yaml_filename':controller_yaml}]),
         Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
@@ -24,12 +39,11 @@ def generate_launch_description():
             output='screen',
             parameters=[{'use_sim_time': True},
                         {'autostart': True},
-                        {'node_names': ['map_server']}]),
-        Node(
-            package='nav2_planner'
-            executable='planner_server'
-            output='screen'
-        ),
+                        {'node_names': ['map_server',
+                                        'bt_navigator',
+                                        'planner_server',
+                                        'controller_server',
+                                        ]}]),
         Node(
             package='turtlebot3_navigation',
             executable='example_node',
